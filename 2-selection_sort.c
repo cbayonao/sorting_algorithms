d0ded1755af782007a272f3aad805fdb6b00f613
mmanumos <mosqueramanuel5@gmail.com>
2020-02-04 10:55:45 +0000

    [Feature] Fixed styles for Betty. modified:   2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,31 +9,31 @@
 
 void selection_sort(int *array, size_t size)
 {
-        unsigned int i, k;
-        int temp = 0, pos = 0;
+	unsigned int i, k;
+	int temp = 0, pos = 0;
 
-        if (array == NULL || size == 0)
-                return;
+	if (array == NULL || size == 0)
+		return;
 
-        for (i = 0; i < (size - 1); i++)
-        {
-                pos = i;
+	for (i = 0; i < (size - 1); i++)
+	{
+		pos = i;
 
-                for (k = i + 1; k < size; k++)
-                {
-                        if (array[k] < array[pos])
-                        {
-                                pos = k;
-                        }
-                }
+		for (k = i + 1; k < size; k++)
+		{
+			if (array[k] < array[pos])
+			{
+				pos = k;
+			}
+		}
 
-                if (array[pos] == array[i])
-                        continue;
+		if (array[pos] == array[i])
+			continue;
 
-                temp = array[pos];
-                array[pos] = array[i];
-                array[i] = temp;
-                print_array(array, size);
-        }
+		temp = array[pos];
+		array[pos] = array[i];
+		array[i] = temp;
+		print_array(array, size);
+	}
 
 }
