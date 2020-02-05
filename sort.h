98211860cda9c256afa274de59457b3ea0126f7d
mmanumos <mosqueramanuel5@gmail.com>
2020-02-05 22:03:00 +0000

    [Feature] Adding the new prototype. modified:   sort.h

--- sort.h
+++ sort.h
@@ -26,6 +26,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void  nuth(int *a, int size, int n);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
