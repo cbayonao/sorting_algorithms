260ae5140b73e142c33fb4178d1231ab69525aeb
mmanumos <mosqueramanuel5@gmail.com>
2020-02-05 21:40:04 +0000

    [Feature] Implement the shell sort algortihm. modified:   100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,8 +1,43 @@
 #include "sort.h"
+
 /**
  *
  *
  *
  *
  */
+
 void shell_sort(int *array, size_t size)
+{
+	size_t inter = 1;
+
+	while (inter < size)
+	{
+		inter = (inter * 3) + 1;
+	}
+
+	while ( (inter = (inter -1) / 3) > 0)
+	{
+		nuth(array, size, inter);
+	}
+
+}
+
+void  nuth(int *a, int size, int n)
+{
+	int tmp, i, j;
+
+	for (i = 0; (i + n) < size; i++)
+	{
+		for (j = i + n; (j - n) >= 0; j = j - n)
+		{
+			if (a[j] < a[j - n])
+			{
+				tmp = a[j];
+				a[j] = a[j - n];
+				a[j - n] = tmp;
+			}
+		}
+	}
+	print_array(a, size);
+}
